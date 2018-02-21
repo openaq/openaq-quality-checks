07871d75c2cb0b1096cbdcdf178b1bdc177a1082
Aimee Barciauskas <aimee@developmentseed.org>
2018-02-21 11:17:11 -0500

    Simple command line functionality

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 #!/usr/bin/env node
+var fs = require('fs');
+
+// cat examples/simple.json | ./index.js
+// ./index.js --infile examples/simple.json
 const argv = require('yargs')
-    .usage('Usage: $0 <command> [options]')
-    .command('count', 'Count the lines in a file')
-    .example('$0 count -f foo.js', 'count the lines in the given file')
-    .alias('f', 'file')
-    .nargs('f', 1)
-    .describe('f', 'Load a file')
-    .demandOption(['f'])
+    .usage('Usage: $0 [options]')
+    .example('$0 --infile foo.js', 'writes the contents given file to stdout')
+    .nargs('--infile', 1)
+    .describe('--infile', 'Input file, json or csv')
     .help('h')
     .alias('h', 'help')
-    .epilog('copyright 2015')
+    .epilog('copyright 2018')
     .argv;
+
+let data = '';
+
+if (argv.infile) {
+  data = fs.readFileSync(argv.infile, 'utf8');
+} else {
+  process.stdin.setEncoding('utf8');
+
+  process.stdin.on('readable', function() {
+    const chunk = this.read();
+    if (chunk !== null) {
+      data += chunk;
+    }
+  });
+
+  process.stdin.on('end', function() {
+    console.log(data);
+  });
+}

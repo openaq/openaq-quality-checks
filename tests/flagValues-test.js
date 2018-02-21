2289d1bb6d5dbaa3887319ba56fbe95274e7ddf4
Aimee Barciauskas <aimee@developmentseed.org>
2018-02-21 14:39:11 -0500

    Data as argument to Flagger#flag

--- tests/flagValues-test.js
+++ tests/flagValues-test.js
@@ -2,29 +2,25 @@ const test = require('ava');
 
 const { Flagger, ArgumentError } = require('../lib/flagger');
 
-const commonProperties = {
-  data: [],
-  flag: 'F'
+const commonProperties = {flag: 'F'};
+const exactFlaggerProperties = {...commonProperties, type: 'exact'};
+const setFlaggerProperties = {...commonProperties, type: 'set'};
+const rangeFlaggerProperties = { ...commonProperties, type: 'range' };
+const rangeFlaggerPropertiesWithEnds = { 
+  ...rangeFlaggerProperties,
+  start: {},
+  end: {}
 };
+const data = [{value: 1}, {value: 0}, {value: 1}, {value: '1'}];
 
 // describe 'flagger#init'
-test('requires a data property', t => {
-  const error = t.throws(() => new Flagger(), ArgumentError);
-  t.is(error.message, 'Missing required argument data.');
-});
-
-test('requires data property to be an array', t => {
-  const error = t.throws(() => new Flagger({data: 'not an array'}), TypeError);
-  t.is(error.message, 'data is not required type Array.');
-});
-
 test('requires a flag property', t => {
-  const error = t.throws(() => new Flagger({data: []}), ArgumentError);
+  const error = t.throws(() => new Flagger(), ArgumentError);
   t.is(error.message, 'Missing required argument flag.');
 });
 
 test('requires flag property to be a string', t => {
-  const error = t.throws(() => new Flagger({data: [], flag: 1}), TypeError);
+  const error = t.throws(() => new Flagger({flag: 1}), TypeError);
   t.is(error.message, 'flag is not required type String.');
 });
 
@@ -41,38 +37,26 @@ test('requires type to be one of string, set, or array', t => {
 // describe 'exact' type
 // init
 test('requires a value', t => {
-  const error = t.throws(() => new Flagger({...commonProperties, type: 'exact'}), ArgumentError);
+  const error = t.throws(() => new Flagger(exactFlaggerProperties), ArgumentError);
   t.is(error.message, 'Missing required argument value.');
 });
 
 test('requires value to be a number', t => {
-  const flaggerProperties = {
-    ...commonProperties,
-    type: 'exact',
-    value: '1'
-  };
-  const error = t.throws(() => new Flagger(flaggerProperties), TypeError);
+  const error = t.throws(() => new Flagger({...exactFlaggerProperties, value: '1'}), TypeError);
   t.is(error.message, 'value is not required type Number.');
 });
 
-const exactFlaggerProperties = {
-  ...commonProperties,
-  type: 'exact',
-  value: 1
-};
 test('sets properties when valid', t => {
-  const flagger = new Flagger(exactFlaggerProperties);
+  const flagger = new Flagger({...exactFlaggerProperties, value: 1});
   t.is(flagger.config.flag, commonProperties.flag);
   t.is(flagger.config.type, 'exact');
   t.is(flagger.config.value, 1);
 });
 
-// flag
-const data = [{value: 1}, {value: 0}, {value: 1}, {value: '1'}];
-
+// exact flag
 test('flags values which match the value', t => {
-  const flagger = new Flagger({...exactFlaggerProperties, data: data});
-  const updatedData = flagger.flag();
+  const flagger = new Flagger({...exactFlaggerProperties, value: 1});
+  const updatedData = flagger.flag(data);
   t.deepEqual(updatedData[0].flags[0], {flag: 'F'});
   t.is(updatedData[1].flags, undefined);
   t.deepEqual(updatedData[2].flags[0], {flag: 'F'});
@@ -82,17 +66,12 @@ test('flags values which match the value', t => {
 // describe 'set' type
 // init
 test('requires values property', t => {
-  const error = t.throws(() => new Flagger({data: [], flag: '1', type: 'set'}), ArgumentError);
+  const error = t.throws(() => new Flagger(setFlaggerProperties), ArgumentError);
   t.is(error.message, 'Missing required argument values.');
 });
 
 test('requires values to be an Array', t => {
-  const flaggerProperties = {
-    ...commonProperties,
-    type: 'set',
-    values: '1'
-  };
-  const error = t.throws(() => new Flagger(flaggerProperties), TypeError);
+  const error = t.throws(() => new Flagger({...setFlaggerProperties, values: 1}), TypeError);
   t.is(error.message, 'values is not required type Array.');
 });
 
@@ -102,24 +81,16 @@ test.todo('does not flag values which are not in the set')
 
 // describe 'range' type
 // init
-const rangeFlaggerProperties = { ...commonProperties, type: 'range' };
-
 test('requires a start object', t => {
   const error = t.throws(() => new Flagger(rangeFlaggerProperties), ArgumentError);
   t.is(error.message, 'Missing required argument start.');
 });
 
 test('requires an end object', t => {
-  const properties = { ...rangeFlaggerProperties, start: {} };
+  const properties = {...rangeFlaggerProperties, start: {}};
   const error = t.throws(() => new Flagger(properties), ArgumentError);
   t.is(error.message, 'Missing required argument end.');
 });
-
-const rangeFlaggerPropertiesWithEnds = { 
-  ...rangeFlaggerProperties,
-  start: {},
-  end: {}
-};
 
 test('requires a start value', t => {
   const error = t.throws(() => new Flagger(rangeFlaggerPropertiesWithEnds), ArgumentError);

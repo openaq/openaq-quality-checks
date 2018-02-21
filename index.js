2114ae00dc519526c5e19df68c2463bf813b7f23
Aimee Barciauskas <aimee@developmentseed.org>
2018-02-21 10:46:45 -0500

    All the options for yargs

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
-const argv = require('yargs').argv
-
-// npm run main --ships=4 --distance=22
-if (argv.ships > 3 && argv.distance < 53.5) {
-  console.log('Plunder more riffiwobbles!')
-} else {
-  console.log('Retreat from the xupptumblers!')
-}
+const argv = require('yargs')
+    .usage('Usage: $0 <command> [options]')
+    .command('count', 'Count the lines in a file')
+    .example('$0 count -f foo.js', 'count the lines in the given file')
+    .alias('f', 'file')
+    .nargs('f', 1)
+    .describe('f', 'Load a file')
+    .demandOption(['f'])
+    .help('h')
+    .alias('h', 'help')
+    .epilog('copyright 2015')
+    .argv;

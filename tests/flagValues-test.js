c75f3d513d5c9ca617224099b8e552f393d9a9dc
Aimee Barciauskas <aimee@developmentseed.org>
2018-02-21 12:56:51 -0500

    More flagger validation

--- tests/flagValues-test.js
+++ tests/flagValues-test.js
@@ -23,18 +23,69 @@ test('requires flag property to be a string', t => {
   t.is(error.message, 'flag is not required type String.');
 });
 
-test.todo('requires a type')
+test('requires a type', t => {
+  const error = t.throws(() => new Flagger({data: [], flag: '1'}), ArgumentError);
+  t.is(error.message, 'Missing required argument type.');
+});
+
+test('requires type to be one of string, set, or array', t => {
+  const error = t.throws(() => new Flagger({data: [], flag: '1', type: 'cupcake'}), TypeError);
+  t.is(error.message, 'type is not one of exact, set, range.');
+});
 
 // describe 'exact' type
 // init
-test.todo('requires a value')
+test('requires a value', t => {
+  const error = t.throws(() => new Flagger({data: [], flag: '1', type: 'exact'}), ArgumentError);
+  t.is(error.message, 'Missing required argument value.');
+});
+
+test('requires value to be a number', t => {
+  const flaggerProperties = {
+    data: [],
+    flag: '1',
+    type: 'exact',
+    value: '1'
+  };
+  const error = t.throws(() => new Flagger(flaggerProperties), TypeError);
+  t.is(error.message, 'value is not required type Number.');
+});
+
+test('sets properties when valid', t => {
+  const flaggerProperties = {
+    data: [],
+    flag: '1',
+    type: 'exact',
+    value: 1
+  };
+  const flagger = new Flagger(flaggerProperties);
+  t.is(flagger.config.flag, '1');
+  t.is(flagger.config.type, 'exact');
+  t.is(flagger.config.value, 1);
+});
+
 // flag
 test.todo('flags values which match the value')
 test.todo('does not flag values which do not match the value')
 
 // describe 'set' type
 // init
-test.todo('requires a values array of values to flag')
+test('requires values property', t => {
+  const error = t.throws(() => new Flagger({data: [], flag: '1', type: 'set'}), ArgumentError);
+  t.is(error.message, 'Missing required argument values.');
+});
+
+test('requires values to be an Array', t => {
+  const flaggerProperties = {
+    data: [],
+    flag: '1',
+    type: 'set',
+    values: '1'
+  };
+  const error = t.throws(() => new Flagger(flaggerProperties), TypeError);
+  t.is(error.message, 'values is not required type Array.');
+});
+
 // flag
 test.todo('flags values which match any of the set')
 test.todo('does not flag values which are not in the set')

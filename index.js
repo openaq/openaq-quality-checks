b81312e47f5ca3b936c57822a337e4b58130c0f5
Aimee Barciauskas <aimee@developmentseed.org>
2018-02-23 15:36:39 -0500

    add command line args documentation and write to file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const parse = require('csv-parse/lib/sync');
+const stringify = require('csv-stringify/lib/sync')
 const yaml = require('js-yaml');
 
 const defaultConfig = yaml.safeLoad(fs.readFileSync('./config.yml', 'utf8'));
@@ -12,7 +13,13 @@ const argv = require('yargs')
     .usage('Usage: $0 [options]')
     .example('$0 --infile foo.json', 'flags the contents of given file and write to stdout')
     .nargs('--infile', 1)
-    .describe('--infile', 'Input file, json or csv')
+    .describe('infile', 'Location of input file, expected json or csv format')
+    .nargs('--outfile', 1)
+    .describe('outfile', 'Location of output file to write to, expected json or csv format')
+    .nargs('--input-format', 1)
+    .describe('input-format', 'Format of input, can be csv or json, defaults to json')
+    .nargs('--ouptput-format', 1)
+    .describe('ouptput-format', 'Format of output, can be csv or json, defaults to json')
     .help('h')
     .alias('h', 'help')
     .epilog('copyright 2018')
@@ -41,24 +48,32 @@ function parseData(data) {
   return parsedData;  
 }
 
-if (argv.infile) {
-  data = fs.readFileSync(argv.infile, 'utf8');
+function readFlagOutput(data) {
   const parsedData = parseData(data);
   const flaggedData = flagData(parsedData);
-  console.log(JSON.stringify(flaggedData));
+  const outData = (argv['output-format'] === 'csv') ? stringify(flaggedData, {header: true}) : JSON.stringify(flaggedData);
+  if (argv.outfile) {
+    fs.writeFileSync(argv.outfile, outData);
+    console.log(`Flagged data written to file ${argv.outfile}`);
+  } else {
+    console.log(outData);
+  }
+}
+
+if (argv.infile) {
+  data = fs.readFileSync(argv.infile, 'utf8');
+  readFlagOutput(data);
 } else {
   process.stdin.setEncoding('utf8');
 
-  process.stdin.on('readable', function() {
+  process.stdin.on('readable', () => {
     const chunk = this.read();
     if (chunk !== null) {
       data += chunk;
     }
   });
 
-  process.stdin.on('end', function() {
-    const parsedData = parseData(data);
-    const flaggedData = flagData(parsedData);
-    console.log(JSON.stringify(flaggedData));
+  process.stdin.on('end', () => {
+    readFlagOutput(data);
   });
 }

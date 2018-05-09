c213e4ae2540336bd7a5335bef56d0c48b776218
Aimee Barciauskas <aimee@developmentseed.org>
2018-05-09 12:20:40 -0400

    Update comment

--- index.js
+++ index.js
@@ -54,7 +54,9 @@ function flagData(data) {
       flaggedData = flagger.flag(flaggedData);
     }
   });
-  // start with each item having an empty flags array, so that csv headers work.
+  // Every item needs a flags array, so that csv output will contain all
+  // headers. Without an empty flags array, if the first row doesn't have flags
+  // no flags will be output since the flags column won't be unrecognized.
   flaggedData.forEach(d => d.flags = d.flags || []);
   return flaggedData;
 }
